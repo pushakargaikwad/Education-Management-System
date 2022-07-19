ac1cb079a25da8b1f0cd0d1ee6fed10520ac8803
Pushakar Gaikwad <pushakargaikwad@gmail.com>
2022-07-19 20:49:24 +0530

    [IMP] Ability to deactivate student records

--- student/models/student.py
+++ student/models/student.py
@@ -9,3 +9,4 @@ class Student(models.Model):
     name = fields.Char(string='Name', required=True)
     roll_number = fields.Char(string='Roll Number')
     language = fields.Many2one('res.lang', string='Mother Tongue')
+    active = fields.Boolean(string='Active', default=True, help='If unchecked, it will allow you to hide the student record without removing it.')
